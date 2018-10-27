1f30b42accf22a5845c5eb6b7250ca4db9d750aa
Suyash Yadav <suyashy929@gmail.com>
2018-10-27 12:53:39 +0530

    Update NavigationActivity.java

--- app/src/main/java/com/a2017002/optimustechproject/optimus_tech_project_2017002/Activity/NavigationActivity.java
+++ app/src/main/java/com/a2017002/optimustechproject/optimus_tech_project_2017002/Activity/NavigationActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.util.Log;
